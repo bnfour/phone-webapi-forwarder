1fc273ebc594412a6e1bce9d61bb25104254101e
Bn4 <bnfour@bnfour.net>
2018-09-02 20:48:31 +0700

    initial android studio project created. So it begins.

---
+++ app/src/main/java/net/bnfour/phone2web2tg_forwarder/MainPreferencesActivity.java
@@ -0,0 +1,12 @@
+package net.bnfour.phone2web2tg_forwarder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainPreferencesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
