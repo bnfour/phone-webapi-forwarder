3fbbd4e4fe3cea722cef4821c10767f250331f72
Bn4 <bnfour@bnfour.net>
2018-09-02 21:28:10 +0700

    app's main activity now contains preferences' skeleton

--- app/src/main/java/net/bnfour/phone2web2tg_forwarder/MainPreferencesActivity.java
+++ app/src/main/java/net/bnfour/phone2web2tg_forwarder/MainPreferencesActivity.java
@@ -8,5 +8,9 @@ public class MainPreferencesActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
     }
 }
---
+++ app/src/main/java/net/bnfour/phone2web2tg_forwarder/SettingsFragment.java
@@ -0,0 +1,39 @@
+package net.bnfour.phone2web2tg_forwarder;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.content.SharedPreferences;
+
+public class SettingsFragment extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getPreferenceManager()
+                .getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        getPreferenceManager()
+                .getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        // TODO update summaries and whatnot
+    }
+}
